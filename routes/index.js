55d99471a97f485031bb4a69b0c9a99a2bdadad6
Shiv Soni <sonishiv309@gmail.com>
2025-10-28 23:02:19 +0530

    find by pk function done

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,10 @@ const express = require('express');
 const routes  = express.Router();
 const findone_routes = require('./find_functions/findone_route');
 const findall_routes = require('./find_functions/findall_routes');
+const findbypk_routes = require('./find_functions/findbypk_routes');
 
 routes.use(findone_routes);
 routes.use(findall_routes);
+routes.use(findbypk_routes);
 
 module.exports = routes;
